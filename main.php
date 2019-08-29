30cc0eb1f6a2cb1f2dce9252162787b71c9b5375
火星大王 <lijunyandeyouxiang@163.com>
2019-08-29 20:49:23 +0800

    加强版，支持枚举查询

--- main.php
+++ main.php
@@ -1,7 +1,9 @@
 <?php
 	error_reporting(0);
-	function chaxun($sno,$time)
+	function chaxun($sno)
 	{
+		$data=(object)[];
+		$data->student_id=$sno;
 		$ch=curl_init('http://27.221.57.108:10009/app/cardInfo');
 		curl_setopt($ch,CURLOPT_HEADER, 0);    
 		curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1); 
@@ -11,22 +13,39 @@
 		$get_sorce=curl_exec($ch);	
 		$stu=json_decode($get_sorce);
 		if($stu->data==NULL)
-			echo '学生'.$sno.'没有绑定卡，嘤嘤嘤'."\n\n\n";
+			$data->code=false;
 		else
 		{
-			echo '学生:'.$stu->data->name."\t卡号:".$stu->data->cardNo."\t余额:".$stu->data->amount."\n";
+			$data->code=true;
+			$data->name=$stu->data->name;
+			$data->card_id=$stu->data->cardNo;
+			$data->amount=$stu->data->amount;
+			$data->logs=[];
 			$ch=curl_init('http://27.221.57.108:10009/app/trades');
 			curl_setopt($ch,CURLOPT_HEADER, 0);    
 			curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1); 
 			curl_setopt($ch,CURLOPT_FOLLOWLOCATION, 1);
 			curl_setopt($ch,CURLOPT_HTTPHEADER,array('Content-Type:application/json'));	
-			curl_setopt($ch,CURLOPT_POSTFIELDS,json_encode(array('sno'=>$sno,'userType'=>0,'pageNum'=>1,'pageSize'=>1000,'timeType'=>$time)));	
+			curl_setopt($ch,CURLOPT_POSTFIELDS,json_encode(array('sno'=>$sno,'userType'=>0,'pageNum'=>1,'pageSize'=>1000)));	
 			$get_sorce=curl_exec($ch);	
 			$card=json_decode($get_sorce);
-			echo $time.'交易记录如下:(共'.count($card->data->trades)."条)\n";
 			foreach($card->data->trades as $trade)
-				var_dump($trade);
+				$data->logs[]=(object)array('time'=>$trade->time,'consumtype'=>$trade->consumType,'amount'=>(float)($trade->amount),'type'=>$trade->type,'mercname'=>$trade->mercName,'mercaccount'=>$trade->mercAccount);
 		}
+		return $data;
+	}
+	function printt($data)
+	{
+		if($data->code)
+		{
+			echo '学生'.$data->student_id."\t姓名:".$data->name."\t卡号:".$data->card_id."\t余额:".$data->amount."\n";
+			echo '交易记录如下:(共'.count($data->logs)."条)\n";
+			foreach($data->logs as $log)
+				echo "\t".$data->name."\t".$log->time."\t".$log->consumtype."\t".$log->amount."元\n";
+		}
+		else
+			echo '学生'.$data->student_id."\t没有绑定卡，嘤嘤嘤\n";
+		
 	}
 	if(($_SERVER['PHP_SELF'])==(end(explode('\\',__FILE__))))
-		chaxun($argv[1],$argv[2]);
+		printt(chaxun($argv[1]));
---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+	include_once('main.php');
+	$cnt=0;
+	$ok=0;
+	$okdata=[];
+	$datas=[];
+	for($i=0;$i<10;$i++)
+	{
+		$buf=chaxun('2017370201880230'.str_pad($i,3,"0",STR_PAD_LEFT));
+		$cnt++;
+		printt($buf);
+		$ok+=$buf->code;
+		if($buf->code)
+			$okdata[]=$buf;
+		$datas[]=$buf;
+	}
+	echo "\n\n\n\n\n\n".'有效数据'.$ok.'条,共'.$cnt.'条,比例:'.($ok/$cnt*100)."%\n";
+	foreach($okdata as $buf)
+		printt($buf);
+	
