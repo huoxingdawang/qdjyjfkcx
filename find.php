b00b977c9dce83216ca491ca4b8e5666c3d648eb
火星大王 <lijunyandeyouxiang@163.com>
2019-08-29 21:01:48 +0800

    增加查询功能

--- find.php
+++ find.php
@@ -1,10 +1,11 @@
 <?php
 	include_once('main.php');
 	$filename='result.json';
-	if($argv[1]!='')
-		$filename=$argv[1];
+	if($argv[2]!='')
+		$filename=$argv[2];
 	$file=fopen($filename, "r") or die("Unable to open file!");
 	$data=json_decode(fread($file,filesize($filename)));
 	fclose($file);
 	foreach($data as $buf)
-		printt($buf);
+		if($buf->code&&$buf->name==$argv[1])
+			printt($buf);
