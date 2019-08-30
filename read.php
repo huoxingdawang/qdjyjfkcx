058ae5d79fc1106e9d018cbba092c8473a48570c
火星大王 <lijunyandeyouxiang@163.com>
2019-08-30 10:52:14 +0800

    我也不知道改了啥。。。

--- read.php
+++ read.php
@@ -3,7 +3,7 @@
 	$filename='result.json';
 	if($argv[1]!='')
 		$filename=$argv[1];
-	$file=fopen($filename, "r") or die("Unable to open file!");
+	$file=fopen($filename, "r") or die("Unable to open file ".$filename);
 	$data=json_decode(fread($file,filesize($filename)));
 	fclose($file);
 	foreach($data as $buf)
