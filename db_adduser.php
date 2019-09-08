82e2b665ba0c07a0fb553798e9a6e62a2c519139
火星大王 <lijunyandeyouxiang@163.com>
2019-09-08 15:10:36 +0800

    调整自动加人算法

--- db_adduser.php
+++ db_adduser.php
@@ -2,9 +2,17 @@
 	include_once('main.php');
 	include_once('db_insert.php');
 	$conn=jry_wb_connect_database();	
+	$ps=1000;
 	for($i=1;$i<=800;$i++)
 	{
-		$buf=chaxun('2018370201880130'.str_pad($i,3,"0",STR_PAD_LEFT));
-		printt($buf);
-		db_insert($conn,$buf);
+		$pn=1;
+		$buff=$ps;
+		echo ($xjh='2017370201880930'.str_pad($i,3,"0",STR_PAD_LEFT))."\n";
+		while($buff>=$ps)
+		{
+			$buf=chaxun($xjh,$ps,$pn);
+			printt($buf);
+			$buff=db_insert($conn,$buf);
+			$pn++;
+		}
 	}
