4849eca6d7c34c395e3ecf027df7d50e8372a87f
火星大王 <lijunyandeyouxiang@163.com>
2019-09-08 15:08:48 +0800

    九中适配

--- main.php
+++ main.php
@@ -7,6 +7,7 @@
 		if		($sno/100000%100==1)	$port=10013	;//一中
 		else if	($sno/100000%100==2)	$port=10009	;//二中
 		else if	($sno/100000%100==15)	$port=9000	;//十五中
+		else if	($sno/100000%100==9)	$port=10021	;//九中
 		else							$port=10009	;
 		$ch=curl_init('http://27.221.57.108:'.$port.'/app/cardInfo');
 		curl_setopt($ch,CURLOPT_HEADER, 0);    
