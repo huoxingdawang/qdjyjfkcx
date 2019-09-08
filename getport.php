727eb2f3846b2c15bc46db9acfa90588eec588cc
火星大王 <lijunyandeyouxiang@163.com>
2019-09-08 15:09:46 +0800

    增加自动暴力枚举端口号

---
+++ getport.php
@@ -0,0 +1,22 @@
+<?php
+	echo ($xjh=$argv[1])."\n"; 
+	for($port=10015;$port<0XFFFF;$port++)
+	{
+		echo "try ".$port."\n";
+		$ch=curl_init('http://27.221.57.108:'.$port.'/app/cardInfo');
+		curl_setopt($ch,CURLOPT_HEADER, 0);
+		 curl_setopt($ch, CURLOPT_TIMEOUT,1);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1); 
+		curl_setopt($ch,CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($ch,CURLOPT_HTTPHEADER,array('Content-Type:application/json'));	
+		curl_setopt($ch,CURLOPT_POSTFIELDS,json_encode(array('sno'=>$xjh,'userType'=>0)));	
+		$get_sorce=curl_exec($ch);	
+		if($get_sorce!='')
+		{
+			echo $get_sorce;
+			$stu=json_decode($get_sorce);
+			if($stu->data!=NULL)
+				return ;
+		}
+	}
+	
